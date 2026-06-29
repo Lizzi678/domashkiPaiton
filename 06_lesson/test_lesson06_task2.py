60197b748b9cdc78e058f6139e19e5d228a5e0b4
Lizzi678 <dostoevskylis66@gmail.com>
2026-06-29 13:45:22 +0700

    Выполнена домашняя работа 6

---
+++ 06_lesson/test_lesson06_task2.py
@@ -0,0 +1,67 @@
+
+import time
+import pytest
+from selenium import webdriver
+
+
+def test_session_storage_auth():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+
+    user_1_cookies = [
+        {
+            "name": "SESSION",
+            "value": "ZWQyZTc0NGUtMDEwZC00NDM3LTg5YzItYTdlYzkwNjYxZjU3",
+        },
+        {
+            "name": "remember-me",
+            "value": "R1p0VVltdnlEaUwxOUVIVjZKVEVBQSUzRCUzRDpIT09RNllvVlpRTk9Xa1VROVhiS0Z3JTNEJTNE",
+        },
+    ]
+
+    user_2_cookies = [
+        {
+            "name": "SESSION",
+            "value": "YjcxOWQ3YjItYzg1Zi00MTYyLWIxZWYtZTg5NjNiOTgwMGZh",
+        }
+    ]
+
+    try:
+        driver.get("https://gitflic.ru/")
+
+        for cookie in user_1_cookies:
+            driver.add_cookie(cookie)
+
+        driver.refresh()
+
+        driver.get("https://gitflic.ru/profile")
+        time.sleep(2)
+
+        url_user_1 = driver.current_url
+        print(f"\nURL Пользователя 1: {url_user_1}")
+
+        driver.delete_all_cookies()
+
+        driver.get("https://gitflic.ru/")
+
+        for cookie in user_2_cookies:
+            driver.add_cookie(cookie)
+
+        driver.refresh()
+
+        driver.get("https://gitflic.ru/profile")
+        time.sleep(2)
+
+        url_user_2 = driver.current_url
+        print(f"URL Пользователя 2: {url_user_2}")
+
+        assert (
+            url_user_1 != url_user_2
+        ), f"Ошибка: URL пользователей одинаковые! {url_user_1} == {url_user_2}"
+
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", "-s", __file__])
